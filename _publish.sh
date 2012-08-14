b6697529bb1cdf7c28e84ae314537c97c27487a7
Kyle Simek <ksimek@email.arizona.edu>
2012-08-13 23:48:28 -0700

    committing first deploy

--- _publish.sh
+++ _publish.sh
@@ -8,9 +8,9 @@
 # Push master branch
 jekyll --pygments \
 && git checkout master \
-&& git rm -r * >/dev/null \
-&& cp -r _site/* . \
-&& rm -r _site 
-git add -A \
+&& git rm -r * >/dev/null 
+cp -r _site/* . \
+&& rm -r _site  \
+&& git add -A \
 && git commit \
 && git push origin master
