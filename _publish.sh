e43d7bc080bd49175622a2eab41efafd5f4b9a2c
Kyle Simek <ksimek@email.arizona.edu>
2012-08-13 23:38:07 -0700

    fixing publish script

---
+++ _publish.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+## Do this before publishing:
+# git checkout source
+# git add -A
+# git commit
+# git push origin source
+
+# Push master branch
+jekyll --pygments \
+&& git checkout master \
+&& git rm -r . >/dev/null \
+&& cp -r _site/* . \
+&& rm -r _site \
+&& git add -A \
+&& git commit \
+&& git push origin master
